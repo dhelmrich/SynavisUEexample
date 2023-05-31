8f689b590276bff17bffb394a234731d0271f487
Dirk Helmrich <dnh2@hi.is>
2023-05-31 16:02:24 +0200

    Sample run.bat for windows/gstreamer (would work the same on unix)

---
+++ Windows/run.bat
@@ -0,0 +1,2 @@
+@echo off
+start .\MinimalWebRTC.exe -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 -RenderOffscreen -ForceRes -ResX=2000 -RexY=1000 -Unattended -PixelStreamingH264Profile=HIGH
