95839413a2511fdf45272018fc32c613b4c1d5f3
Dirk Helmrich <dnh2@hi.is>
2023-06-02 13:52:44 +0200

    Changed gitignore for PXS and added command options

--- Windows/run.bat
+++ Windows/run.bat
@@ -1,3 +1,6 @@
 @echo off
-start .\MinimalWebRTC.exe -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 -RenderOffscreen -ForceRes -ResX=2000 -RexY=1000 -Unattended -PixelStreamingH264Profile=HIGH -PixelStreamingWebRTCDegradationPreference=MAINTAIN_QUALITY
+start .\MinimalWebRTC.exe -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 ^
+ -RenderOffscreen -ForceRes -ResX=2000 -RexY=1000 -Unattended -PixelStreamingH264Profile=HIGH ^
+ -PixelStreamingWebRTCDegradationPreference=MAINTAIN_QUALITY ^
+-ExecCmds="PixelStreaming.WebRTC.DisableTransmitAudio true, PixelStreaming.Encoder.Codec H265, PixelStreaming.WebRTC.DisableAudioSync true, PixelStreaming.AllowPixelStreamingCommands true, PixelStreaming.Encoder.KeyframeInterval 1, PixelStreaming.Encoder.EnableFillerData true, PixelStreaming.WebRTC.DegradationPreference MAINTAIN_QUALITY, PixelStreaming.WebRTC.Fps 10, t.MaxFPS 10"
 
