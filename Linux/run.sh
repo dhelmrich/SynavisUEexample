aed42933b380c4c3cbca9e49ed12f1b8929eb480
Dirk Helmrich <dnh2@hi.is>
2023-06-02 10:11:14 +0200

    Adding run scripts for PixelStreaming for AI
    Can and perhaps should be changed to VP9 for multiple endpoints, as H264 or rather nvencode are restricted in terms of # of sessions.

---
+++ Linux/run.sh
@@ -0,0 +1,5 @@
+./MinimalWebRTC.sh -PixelStreamingIP=127.0.0.1 -PixelStreamingPort=8888 \
+                   -RenderOffscreen -ForceRes -ResX=2000 -RexY=1000     \
+                   -Unattended -PixelStreamingH264Profile=HIGH          \
+                   -PixelStreamingDegradationPreference=MAINTAIN_QUALITY
+
