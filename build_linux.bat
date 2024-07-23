29ef49005d1205b166e9bd5969f2ba22ebb7dae3
Dirk Helmrich <dnh2@hi.is>
2024-07-23 18:21:35 +0200

    Build scripts use env now

--- build_linux.bat
+++ build_linux.bat
@@ -2,5 +2,8 @@
 
 REM store current directory
 set current_dir=%cd%
+set uat_file=%UEPATH%\..\..\Build\BatchFiles\RunUAT.bat
 
-"C:\work\UE_5.2\Engine\Build\BatchFiles\RunUAT.bat"  BuildCookRun -project=%current_dir%\MinimalWebRTC.uproject -noP4 -platform=Linux -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="D:/Packages/photo"
+echo %uat_file%
+
+"%uat_file%" BuildCookRun -project=%current_dir%\MinimalWebRTC.uproject -noP4 -platform=Linux -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=%current_dir% -nocompileeditor
--- build_windows.bat
+++ build_windows.bat
@@ -1,7 +1,11 @@
-@echo off
+REM @echo off
 
 REM store current directory
 set current_dir=%cd%
+set ue_dir=%UEPATH%
+set uat_file=%ue_dir%\..\..\Build\BatchFiles\RunUAT.bat
 
-"C:\work\UE_5.2\Engine\Build\BatchFiles\RunUAT.bat" BuildCookRun -project=%current_dir%\MinimalWebRTC.uproject -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory="D:/Packages/photo"
+echo %uat_file%
+
+"%uat_file%" BuildCookRun -project=%current_dir%\MinimalWebRTC.uproject -noP4 -platform=Win64 -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=%current_dir$ -nocompileeditor
 
