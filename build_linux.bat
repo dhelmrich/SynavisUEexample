280993c4d099a9e3bfd315bcd9f74ada6879b8f5
Dirk Helmrich <dnh2@hi.is>
2024-08-05 11:18:19 +0200

    Removed old files

--- build_linux.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-REM store current directory
-set current_dir=%cd%
-set uat_file=%UEPATH%\..\..\Build\BatchFiles\RunUAT.bat
-
-echo %uat_file%
-
-"%uat_file%" BuildCookRun -project=%current_dir%\MinimalWebRTC.uproject -noP4 -platform=Linux -clientconfig=Development -cook -allmaps -build -stage -pak -archive -archivedirectory=%current_dir% -nocompileeditor
